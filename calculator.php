3be759941685c38b33003876944a21e08c707cb0
Pradeep yadav <pradeep.yadav@enigmaticsmile.com>
2025-04-12 23:22:28 +0530

    Change from feature/conflict-x

--- calculator.php
+++ calculator.php
@@ -1,24 +1,3 @@
 <?php
-echo "This line is written by conflict-1 branch";
-?>
-<?php
-echo "This is logic from conflict-a branch";
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $operation = $_POST['operation'];
-//add
-
-    switch ($operation) {
-		    //Div
-             case 'div':
-             echo "Result : " . ($num2 != 0 ? $num1 / $num2 : "Cannot divide by zero");
-             break;
-            case 'mul':
-            echo "Result: " . ($num1 * $num2);
-            break;
-    }
-}
+echo "This is from conflict A";
 ?>
